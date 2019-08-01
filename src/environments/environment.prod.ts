ae5aff436ffa446f00aeda2c6530167de049a9be
Sean Tayler <seantayler@gAM7acd39.local>
2019-08-01 09:42:23 -0700

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBK_d0jHULRAvDO_7xCMlYcgkmlfQSwxe0",
+    authDomain: "ngshop-48d2c.firebaseapp.com",
+    databaseURL: "https://ngshop-48d2c.firebaseio.com",
+    projectId: "ngshop-48d2c",
+    storageBucket: "",
+    messagingSenderId: "761170945668",
+    appId: "1:761170945668:web:e4da53055a8e6643"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBK_d0jHULRAvDO_7xCMlYcgkmlfQSwxe0",
+    authDomain: "ngshop-48d2c.firebaseapp.com",
+    databaseURL: "https://ngshop-48d2c.firebaseio.com",
+    projectId: "ngshop-48d2c",
+    storageBucket: "",
+    messagingSenderId: "761170945668",
+    appId: "1:761170945668:web:e4da53055a8e6643"
+  }
 };
 
 /*
